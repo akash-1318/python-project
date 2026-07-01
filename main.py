899c759749574d14ad61c5da6799d49495887954
Akash Sharma <akash_18@192.168.1.7>
2026-07-01 20:09:37 +0530

    done

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from fastapi import FastAPI
+# import requests
+
+# response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+
+
+# data = response.json()
+# print(data[:2])
+
+from fastapi import FastAPI,HTTPException
+import requests
 
 
 app = FastAPI()
 
+#GET ALL data
+@app.get("/posts")
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    return response.json()
 
-@app.get("/")
-def home():
-    return{
-        "message":"Hello Mohit"
-    }
+#Get single post
+@app.get("/posts/{post_id}")
+def get_post(post_id:int):
+    url = f"https://jsonplaceholder.typicode.com/posts/{post_id}"
 
-@app.get("/add")
-def add(a:int, b:int):
-    return{
-        "result": a+b
-    }
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Page not found")
+    return response.json()
